7ce5489b41ce33208faa0117a31d6ef17946d2af
Nasir2023 <129264930+Nasir2023@users.noreply.github.com>
2023-04-07 12:10:33 +0200

    Add assignment

---
+++ main.py
@@ -0,0 +1,34 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+a='Ruud Gullit'
+b=' scored'
+c=' Marco van Basten'
+d=' in the 32nd minute'
+g=' in the 54th minute'
+goal_0=32
+goal_1=54
+scorers='Ruud Gullit 32, Marco van Basten 54'
+
+
+x='Ruud Gullit scored in the 32nd minute'
+y=' Marco van Basten scored in the 54th minute'
+xy='Ruud Gullit scored in the 32nd minute\nMarco van Basten scored in the 54th minute'
+
+report=(xy)
+first_name="Hennadiy"
+last_name='Koeman'
+player='Hennadiy Lytovchenko'
+b_1=player.find('Hennadiy')
+last_name='Lytovchenko'
+print(len(last_name))
+last_name_len=11
+name_short='H. Lytovchenko'
+print(2 != 2)
+print(2 != 3)
+chant=('Hennadiy! ''Hennadiy! ''Hennadiy! ''Hennadiy! ''Hennadiy! ''Hennadiy! ''Hennadiy! ''Hennadiy!')
+print(chant)
+good_chant=(chant != 'Hennadiy!')
+print(good_chant )
